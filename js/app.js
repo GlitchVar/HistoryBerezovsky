d6fc7685c3f5fd03fd23703d326ac363dccaa856
GlitchVar <155877542+GlitchVar@users.noreply.github.com>
2024-09-21 16:29:12 +0500

    app.js

--- js/app.js
+++ js/app.js
@@ -87,6 +87,7 @@ function openModal(event) {
     window.addEventListener("keydown", (event) => {
         if(event.key == "Escape") {
             modal.style.display = 'none';
+            document.body.style.overflow = 'auto';
         }
     })
     const buttonClose = modalContent.querySelector('.close-btn');
